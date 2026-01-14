1d09612e64cbcdb6fac025074abf40ed90768f8a
Hugo <hugoramos1466@gmail.com>
2026-01-14 09:50:16 +0100

    debug

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 first_name = "Holberton" # stock "Holberton" dans la variable first_name 
 last_name = "School" # stock "School" dans la variable last_name 
 full_name = first_name + " " + last_name # stock le resultat de la concatenation de first_name et last_name dans la variable full_name 
