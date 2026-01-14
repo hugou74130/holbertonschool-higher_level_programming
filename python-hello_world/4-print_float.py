c7102d95e4651a5e126c3ef58080928bf8be51cf
Hugo <hugoramos1466@gmail.com>
2026-01-14 09:49:04 +0100

    Update shebang in 4-print_float.py to use /usr/bin/python3

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 number = 3.14159 # stock 3.14159 dans la variable number 
 print(f"{number:.2f}") # affiche le resultat dans une chaine de caractere formater avec 2 decimales 
