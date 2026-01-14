8f96395b6cf97a0691670f67cfad9d5b6216c05d
Hugo <hugoramos1466@gmail.com>
2026-01-14 14:56:45 +0100

    Remove unnecessary blank line and call fizbuzz function in 12-fizzbuzz.py for improved functionality

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def fizbuzz():
-    
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
@@ -10,3 +9,6 @@ def fizbuzz():
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
+
+
+fizbuzz()
