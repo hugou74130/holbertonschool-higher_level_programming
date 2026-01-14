8927c993843c6e68e4dea293ce362b57197814f5
Hugo <hugoramos1466@gmail.com>
2026-01-14 12:03:26 +0100

    Refactor string handling in 2-print_alphabet.py to enhance clarity and maintain consistent output formatting

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        print("{}".format(i), end="")
+        print("{}".format(j), end="")
+        if i != 8 or j != 9:
+            print(", ", end="")
+        else:
+            print("")
