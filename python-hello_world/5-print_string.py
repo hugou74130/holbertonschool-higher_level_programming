8f94efb3ee5f1010b5a2427b614aac59627060ec
Hugo <hugoramos1466@gmail.com>
2026-01-14 10:24:12 +0100

    Refactor print statement in 5-print_string.py to display repeated string and substring

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-first_name = "Holberton"
-last_name = "School"
-full_name = first_name + " " + last_name
-print(f"Welcome to {full_name}!")
+str = "Holberton School"
+print(f"{str * 3}")
+print(str[:9])
