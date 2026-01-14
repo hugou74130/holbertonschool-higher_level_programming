2507613ebc8d30e1f9348b0bb36d7addfa162715
Hugo <hugoramos1466@gmail.com>
2026-01-14 13:47:08 +0100

    Refactor number printing logic in 5-print_comb2.py to simplify output generation and enhance formatting consistency

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        print("{}".format(i), end="")
-        print("{}".format(j), end="")
-        if i != 8 or j != 9:
-            print(", ", end="")
-        else:
-            print("")
+for i in range(99):
+    print("{:02d}, ".format(i), end="")
+print("99")
