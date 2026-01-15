2c4d75847fd0dc6c0bded33474ddfe1a8e0b3994
Hugo <hugoramos1466@gmail.com>
2026-01-15 15:43:11 +0100

    Refactor 0-add.py to include a main block for testing the add function and remove 0-import_add.py as it is no longer needed

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
-def add(a, b):
+if __name__ == "__main__":
+    from add_0 import add
 
-    return (a + b)
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
--- python-import_modules/0-import_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def add(a, b):
-
-    a = 1
-    b = 2
-
-    print(f"{a} + {b} = {add(a, b)}")
