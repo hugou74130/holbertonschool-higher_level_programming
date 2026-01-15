6216968cfa79488098339f157b0dcc0f92a00644
Hugo <hugoramos1466@gmail.com>
2026-01-15 17:45:15 +0100

    Fix argument count messages and improve output formatting in 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,12 +3,13 @@ from sys import argv
 
 if __name__ == "__main__":
     nb_args = len(argv) - 1
+
     if nb_args == 0:
         print("0 arguments.")
-    elif nb_args == - 1:
-        print(f"1 arguemnts:")
+    elif nb_args == 1:
+        print(f"1 argument:")
     else:
-        print(f"{nb_args} argumetns:")
+        print(f"{nb_args} arguments:")
+
     for i in range(1, len(argv)):
-        position = i
-        print(f"{position}:{argv[i]}")
+        print(f"{i}: {argv[i]}")
