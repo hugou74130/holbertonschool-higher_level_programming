a9b723a2c5ed5e98c28db50c7dab416fa34a1b54
Hugo Ramos <hugoramos1466@gmail.com>
2026-03-24 15:24:04 +0100

    5. Change the text

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+document.querySelector("header").style.color = "#FF0000";
+document.getElementById("update_header").addEventListener("click", function() {
+    document.querySelector("header").textContent = "New Header!!!";
+});
