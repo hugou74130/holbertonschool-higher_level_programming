1766b944e01c08d01302cddcef5cc2d8c787e975
Hugo Ramos <hugoramos1466@gmail.com>
2026-03-24 13:58:20 +0100

    1. Click and turn red

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+document.querySelector("header").style.color = "#FF0000";
+document.querySelector("#red_header").addEventListener("click", function() {
+    document.querySelector("header").style.color = "#FF0000";
+});
